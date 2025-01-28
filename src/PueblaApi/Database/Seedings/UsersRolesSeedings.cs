9147f63ef56e9916cf873faa8a85c38600133b59
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-01-28 00:34:37 -0600

    More user seedings
    Did not add this to a particular issue/task/bug as it was not necessary.

--- src/PueblaApi/Database/Seedings/UsersRolesSeedings.cs
+++ src/PueblaApi/Database/Seedings/UsersRolesSeedings.cs
@@ -66,5 +66,45 @@ public class UsersRolesSeedings
                 await userManager.AddToRoleAsync(admin, roleNames[0]);
             }
         }
+
+
+        // 4. Seed normal users (3 managers and 7 users).
+        await SeedUser(userManager, "john.doe@example.com", "johndoe", "John", "Doe", roleNames[1]);
+        await SeedUser(userManager, "jane.smith@example.com", "janesmith", "Jane", "Smith", roleNames[1]);
+        await SeedUser(userManager, "alex.johnson@example.com", "alexjohnson", "Alex", "Johnson", roleNames[1]);
+        await SeedUser(userManager, "chris.taylor@example.com", "christaylor", "Chris", "Taylor", roleNames[2]);
+        await SeedUser(userManager, "taylor.anderson@example.com", "tayloranderson", "Taylor", "Anderson", roleNames[2]);
+        await SeedUser(userManager, "jordan.thomas@example.com", "jordanthomas", "Jordan", "Thomas", roleNames[2]);
+        await SeedUser(userManager, "morgan.jackson@example.com", "morganjackson", "Morgan", "Jackson", roleNames[2]);
+        await SeedUser(userManager, "riley.white@example.com", "rileywhite", "Riley", "White", roleNames[2]);
+        await SeedUser(userManager, "parker.harris@example.com", "parkerharris", "Parker", "Harris", roleNames[2]);
+        await SeedUser(userManager, "cameron.martin@example.com", "cameronmartin", "Cameron", "Martin", roleNames[2]);
+    }
+    /// <summary>
+    /// Seeds users with the default 'user' role.
+    /// </summary>
+    /// <param name="email">User's email (automatically confirmed)</param>
+    /// <param name="username">User's username</param>
+    /// <param name="firstName">User's first name</param>
+    /// <param name="lastName">User's last name</param>
+    /// <param name="role">User's role (manager or user)</param>
+    /// <returns></returns>
+    private static async Task SeedUser(UserManager<ApplicationUser> userManager, string email, string username, string firstName, string lastName, string role = ApiRoles.User)
+    {
+        // Password for seeded users.
+        string password = "User#123";
+        ApplicationUser user = new ApplicationUser()
+        {
+            Email = email,
+            UserName = username,
+            FirstName = "admin",
+            LastName = "admin",
+            //NationalId = "0",
+            IsEnabled = true,
+            EmailConfirmed = true,
+        };
+        // If it doesn't exist, create administrator user and assign them the admin role.
+        await userManager.CreateAsync(user, password);
+        await userManager.AddToRoleAsync(user, role);
     }
 }
