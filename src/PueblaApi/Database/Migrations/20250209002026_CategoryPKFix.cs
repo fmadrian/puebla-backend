a46b34d7eef1ae1d447c0ac312c5599b048d9b58
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-02-08 18:47:12 -0600

    Fix primary key conflict when creating new categories [PUEBLA-86]
    Conflict is caused by primary key values colliding with those used for seeded data.
    
    Fixed by setting the starting value for the ID (PK) field to more than the number of seeded items.
    
    Added respective database migration.

---
+++ src/PueblaApi/Database/Migrations/20250209002026_CategoryPKFix.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PueblaApi.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryPKFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Categories",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:IdentitySequenceOptions", "'30', '1', '', '', 'False', '1'")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Categories",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:IdentitySequenceOptions", "'30', '1', '', '', 'False', '1'")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
